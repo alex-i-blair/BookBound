ede72945e5d5c0ded70e069a451ed7ec5ec1675b
Denzel <dbartolaba123@gmail.com>
2022-02-28 11:07:28 -0800

    Added in Redirects for app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Switch, Route, NavLink, Redirect } from 'react-router-dom';
 import AuthPage from './AuthPage';
 import { getUser, logout } from './services/fetch-utils';
+import ReadingList from './ReadingList';
+import SearchPage from './SearchPage';
 
 function App() {
   const [currentUser, setCurrentUser] = useState(null);
@@ -21,8 +23,19 @@ function App() {
     setCurrentUser(null);
   }
   return (
-    <div className="App">
-      <Router>
+    <Router>
+      <div className="App">
+     
+        <header className="App-header">
+          {
+            currentUser && <div className='nav-links'> 
+              <button onClick={handleLogout}>Logout</button>
+                
+            </div>
+                
+          }
+    
+        </header>
         <Switch>
           <Route exact path='/'>
             {
@@ -32,10 +45,25 @@ function App() {
             }
 
           </Route>
+          <Route exact path='/reading-list'>
+            {
+              currentUser ? 
+                <ReadingList /> :
+                <Redirect to='/' />
+            }
+          </Route>
+          <Route exact path='/search-page'>
+            {
+              currentUser ? 
+                <SearchPage /> :
+                <Redirect to='/' />
+            }
+          </Route>
+
         </Switch>
-      </Router>
-      <header className="App-header"></header>
-    </div>
+      
+      </div>
+    </ Router>
   );
 }
 
