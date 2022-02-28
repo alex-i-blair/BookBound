f2398437a63db8d9417244b119e0e35d7c682874
cliffmax85 <cliffmax85@gmail.com>
2022-02-28 11:27:14 -0800

    added endpoint

--- functions/book-endpoint.js
+++ functions/book-endpoint.js
@@ -2,9 +2,12 @@ const fetch = require('node-fetch');
 require('dotenv').config();
 
 
-exports.handler = async (event, context) => {
+exports.handler = async (event) => {
+  const searchQuery = event.queryStringParameters.searchQuery;
+  
+  const URL = `https://www.googleapis.com/books/v1/volumes?q=${searchQuery}&startIndex=0&maxResults=40&orderBy=relevance&key=${process.env.BOOK_API_KEY}`
   try {
-    const response = await fetch('https://cat-fact.herokuapp.com/facts');
+    const response = await fetch(URL);
     const data = await response.json();
     const json = JSON.stringify({ data });
     
