295efd59d6718734b9a17cfca4ce41c5f972f353
Denzel <dbartolaba123@gmail.com>
2022-03-01 11:27:14 -0800

    Added in another endpoint to get a singlebook

---
+++ functions/singleBook-endpoint.js
@@ -0,0 +1,24 @@
+const fetch = require('node-fetch');
+require('dotenv').config();
+
+exports.handler = async (event) => {
+  const searchQuery = event.queryStringParameters.searchQuery;
+
+  const URL = `https://www.googleapis.com/books/v1/volumes/${searchQuery}?projection=full&key=${process.env.BOOK_API_KEY}`;
+  try {
+    const response = await fetch(URL);
+    const data = await response.json();
+    const json = JSON.stringify({ data });
+
+    return {
+      statusCode: 200,
+      body: json,
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Failed fetching data' }),
+    };
+  }
+};
