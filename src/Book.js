c2e05e01ca1d77fe7606261dcb4f6063a7919779
Alex Blair <bclan11@gmail.com>
2022-02-28 16:45:57 -0800

    images and author rendering fixed

--- src/Book.js
+++ src/Book.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Book({ book }) {
-  const authors = book.volumeInfo.authors;
-  const authorString = authors.join(' | ');
+  const authors = [];
+  book.volumeInfo.authors ? authors.push(book.volumeInfo.authors.join(' | ')) : {};
+
   return (
-    <div className="book">
-      <h3>{book.volumeInfo.title}</h3>
-      <p>by: {authorString}</p>
-      <img src={book.volumeInfo.imageLinks.smallThumbnail}></img>
-    </div>
+    <Link to={`/book-details/${book.id}`}>
+      <div className="book">
+        <h3>{book.volumeInfo.title}</h3>
+        <p>by: {authors}</p>
+        <img
+          src={`https://books.google.com/books/content?id=${book.id}&printsec=frontcover&img=1&zoom=5&source=gbs_api`}
+        />
+      </div>
+    </Link>
   );
 }
